7ba5f2b80ee7e15c6bbf898eaf13ef89c76a93dd
CHuang <chuang@zscaler.com>
2018-06-05 05:23:41 -0700

    Add more filtering in display

--- display.py
+++ display.py
@@ -13,10 +13,22 @@ RESULT_DIR = config.RESULT_DIR
 def basic_trend():
     print '=============================================  ETF  ==================================================='
     df = pd.read_csv(RESULT_DIR + 'basic_trend_etf.csv', index_col = 0)
-    print df
+    temp_df = df[(df.swidth_pb > 70) & (df.rwb >= 0) & ((df.macd_hist_pb >= 1) | (df.fwidth_pb >= 1))]
+    print temp_df
     print '=============================================  STOCK ==================================================='
     df = pd.read_csv(RESULT_DIR + 'basic_trend_stock.csv', index_col = 0)
-    print df
+    temp_df = df[(df.swidth_pb > 70) & (df.rwb >= 0) & ((df.macd_hist_pb >= 1) | (df.fwidth_pb >= 1))]
+    print temp_df
+
+def basic_trend_emerging():
+    print '=============================================  ETF  ==================================================='
+    df = pd.read_csv(RESULT_DIR + 'basic_trend_etf.csv', index_col = 0)
+    temp_df = df[(((df.macd_hist_pb >= 1) & (df.macd_hist_pb <= 3)) | ((df.fwidth_pb >= 1) & (df.fwidth_pb <= 3))) & (df.swidth_pb > 70) & (df.rwb >= 0)]
+    print temp_df
+    print '=============================================  STOCK ==================================================='
+    df = pd.read_csv(RESULT_DIR + 'basic_trend_stock.csv', index_col = 0)
+    temp_df = df[(((df.macd_hist_pb >= 1) & (df.macd_hist_pb <= 3)) | ((df.fwidth_pb >= 1) & (df.fwidth_pb <= 3))) & (df.swidth_pb > 70) & (df.rwb >= 0)]
+    print temp_df
 
 # ==============================================================================
 #   Main
@@ -25,6 +37,12 @@ def main(argv):
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
     pd.set_option('display.width', 1000)
+
+    print '\n###################################################     Emerging     ######################################################\n'
+
+    basic_trend_emerging()
+
+    print '\n###################################################       All        ######################################################\n'
 
     basic_trend()
 
