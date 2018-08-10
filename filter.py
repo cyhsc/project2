5ae8d2798cf9df97e58fe22541f9fc7cf59658d4
CHuang <chuang@zscaler.com>
2018-08-10 03:52:05 -0700

    Add stochastic

--- filter.py
+++ filter.py
@@ -31,6 +31,10 @@ class Filter:
         rwb = []        
         macd_hist_pb = []
         swidth_roc_pb = []
+        fwidth_roc_pb = []
+        diff_slow_kd_pb = []
+        slowk = []
+        slowd = []
      
         for sym in symbols: 
             df = utils.read_analysis_file(sym)
@@ -47,6 +51,10 @@ class Filter:
             rwb.append(df['rwb'][-1])
             macd_hist_pb.append(df['macd_hist_pb'][-1])
             swidth_roc_pb.append(df['swidth_roc_pb'][-1])
+            fwidth_roc_pb.append(df['fwidth_roc_pb'][-1])
+            diff_slow_kd_pb.append(df['diff_slow_kd_pb'][-1])
+            slowk.append(df['slowk'][-1])
+            slowd.append(df['slowd'][-1])
 
         align_total_df = pd.DataFrame(index = symbols)
         align_total_df['name'] = names
@@ -59,9 +67,80 @@ class Filter:
         align_total_df['fwidth_pb'] = fwidth_pb
         align_total_df['macd_hist_pb'] = macd_hist_pb
         align_total_df['swidth_roc_pb'] = swidth_roc_pb 
+        align_total_df['fwidth_roc_pb'] = fwidth_roc_pb 
+        align_total_df['diff_slow_kd_pb'] = diff_slow_kd_pb
+        align_total_df['slowk'] = slowk
+        align_total_df['slowd'] = slowd
     
         ret_df = align_total_df.sort_values(['slow_align_total', 'slow_roc_total', 'fast_align_total', 'fast_roc_total', 'swidth_pb'], ascending=[False, False, False, False, False])
 
         ret_df.to_csv(RESULT_DIR + 'basic_trend_' + type + '.csv')
 
         return ret_df
+
+    def obsolete(self): 
+
+        df = utils.read_analysis_file('spy')
+        date = df.index[-1]
+
+        etf_symbols, names = utils.read_symbol_file('symbols_etf.csv')
+        stock_symbols, names = utils.read_symbol_file('symbols_stock.csv')
+        combined_symbols = etf_symbols + stock_symbols
+        obsolete_symbols = []
+
+        for sym in combined_symbols:
+            df = utils.read_analysis_file(sym)
+            if df is None:
+                print 'Analysis file for', sym, ' doesnot exist'
+                continue
+            
+            if df.index[-1] < date: 
+                print sym, 'is obsolete'
+                obsolete_symbols.append(sym)
+
+        print '------------------- Obsoleted Symbols -----------------------'
+        print obsolete_symbols
+        
+    def three_percent(self, type): 
+
+        if type == 'etf':
+            symbol_file = 'symbols_etf.csv'
+        elif type == 'stock':
+            symbol_file = 'symbols_stock.csv'
+        else:
+            print 'Enter valid type, etf or stock'
+            return
+
+        symbols, names = utils.read_symbol_file(symbol_file)
+
+        tp_symbols = []
+        tp_changes = []
+        tp_vchanges = []
+
+        for sym in symbols:
+            df = utils.read_analysis_file(sym)
+            if df is None:
+                print 'Analysis file for', sym, ' doesnot exist'
+                continue
+
+            closes = df['close']
+            if len(closes) < 2:
+                continue
+
+            volumes = df['volume']
+            vol_sma50 = df['vol_sma50']
+            change = (100*(float(closes[-1]) - float(closes[-2])))/float(closes[-2])
+            vchange = (100*(float(volumes[-1]) - float(vol_sma50[-1])))/float(vol_sma50[-1])
+            swidth = df['swidth']
+            if (change >= 3) and (vchange >= 150) and (swidth[-1] > 0): 
+                tp_symbols.append(sym)
+                tp_changes.append(change)
+                tp_vchanges.append(vchange)
+
+        tp_df = pd.DataFrame(index = tp_symbols)
+        tp_df['change'] = tp_changes
+        tp_df['volume_change'] = tp_vchanges
+
+        tp_df.to_csv(RESULT_DIR + 'three_percent_' + type + '.csv')
+
+        return tp_df
